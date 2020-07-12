929a01245e3bd932a4f66312e01be032de120ffc
Lukas Taegert-Atkinson <lukas.taegert-atkinson@tngtech.com>
2020-07-12 20:34:41 +0200

    Support live-bindings for default imports, deconflict default variables

--- test/chunking-form/samples/deprecated/preserve-modules-commonjs/_expected/amd/main.js
+++ test/chunking-form/samples/deprecated/preserve-modules-commonjs/_expected/amd/main.js
@@ -1,9 +1,9 @@
 define(['external', './commonjs'], function (external, commonjs) { 'use strict';
 
-	function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+	function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex : { 'default': ex }; }
 
-	external = _interopDefault(external);
+	var external__default = _interopDefault(external);
 
-	console.log(commonjs.default, external);
+	console.log(commonjs.default, external__default.default);
 
 });
